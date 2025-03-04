74aaebdbbe667da1dcc64ad4c051161c772d3998
antbern <antbern@users.noreply.github.com>
2025-03-04 09:42:50 +0000

    deploy: 644f694dc58a270f43b717ac5c357a9a832c0b78

--- rust-docs/doc/sdk/core/config/sidebar-items.js
+++ rust-docs/doc/sdk/core/config/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["Config","DeserializedConfig"]};
+window.SIDEBAR_ITEMS = {"struct":["Config"]};
--- rust-docs/doc/trait.impl/core/str/traits/trait.FromStr.js
+++ rust-docs/doc/trait.impl/core/str/traits/trait.FromStr.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["sdk",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"struct\" href=\"sdk/core/config/struct.DeserializedConfig.html\" title=\"struct sdk::core::config::DeserializedConfig\">DeserializedConfig</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"struct\" href=\"sdk/share/struct.Share.html\" title=\"struct sdk::share::Share\">Share</a>"]]]]);
+    var implementors = Object.fromEntries([["sdk",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"struct\" href=\"sdk/share/struct.Share.html\" title=\"struct sdk::share::Share\">Share</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[593]}
+//{"start":57,"fragment_lengths":[275]}
