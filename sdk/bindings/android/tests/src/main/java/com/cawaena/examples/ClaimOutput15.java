ETOSPHERES-Labs/cawaena-sdk

update tests,examples,docs after modifying java package name

sdk/bindings/android/tests/src/main/java/com/cawaena/examples/ClaimOutput15.java
@@ -1,0 +1,33 @@
+package com.cawaena.examples;
+
+import com.cawaena.Wallet;
+
+public class ClaimOutput15 {
+
+    public static void main(String[] args) {
+
+        // initialize the sdk
+        Wallet sdk = utils.initSdk(utils.USERNAME_SATOSHI);
+
+        String password = utils.getEnvVariable("PASSWORD");
+
+        try {
+            // create and init user
+            sdk.createNewUser(utils.USERNAME_SATOSHI);
+            sdk.initializeUser(utils.USERNAME_SATOSHI);
+            System.out.println("Created and initialized new user.");
+
+            // create new wallet
+            sdk.setPassword(utils.PIN, password);
+            sdk.createNewWallet(utils.PIN);
+            System.out.println("Created and initialized new wallet.");
+
+            // claim output
+            sdk.claimOutputs(utils.PIN);
+            System.out.println("claim outputs success");
+
+        } catch (Exception e) {
+            throw new RuntimeException("Claim outputs example failed", e);
+        }
+    }
+}

sdk/bindings/android/tests/src/main/java/com/cawaena/examples/MigrateWalletFromBackup05.java
@@ -1,0 +1,39 @@
+package com.cawaena.examples;
+
+import com.cawaena.Wallet;
+public class MigrateWalletFromBackup05 {
+
+    public static void main(String[] args) {
+
+        // initialize the sdk
+        Wallet sdk = utils.initSdk(utils.USERNAME_SATOSHI);
+
+        String password = utils.getEnvVariable("PASSWORD");
+
+        try {
+            // create and init user
+            sdk.createNewUser(utils.USERNAME_SATOSHI);
+            sdk.initializeUser(utils.USERNAME_SATOSHI);
+            System.out.println("Created and initialized new user.");
+
+            // create new wallet
+            sdk.setPassword(utils.PIN, password);
+            sdk.createNewWallet(utils.PIN);
+            System.out.println("Created and initialized new wallet.");
+
+            // create backup
+            byte[] backup_bytes = sdk.createWalletBackup(utils.PIN, "backup_password");
+
+            // delete existing wallet
+            sdk.deleteWallet(utils.PIN);
+            System.out.println("deleted existing wallet");
+
+            // migrate wallet from backup
+            sdk.createWalletFromBackup(utils.PIN, backup_bytes, "backup_password");
+            System.out.println("wallet restored from backup");
+
+        } catch (Exception e) {
+            throw new RuntimeException("Migrate wallet from backup example failed", e);
+        }
+    }
+}

ETOSPHERES-Labs/etopay-sdk

sdk/bindings/android/tests/src/main/java/com/cawaena/examples/ClaimOutput15.java
@@ -1,0 +1,33 @@
+package com.cawaena.examples;
+
+import com.cawaena.Wallet;
+
+public class ClaimOutput15 {
+
+    public static void main(String[] args) {
+
+        // initialize the sdk
+        Wallet sdk = utils.initSdk(utils.USERNAME_SATOSHI);
+
+        String password = utils.getEnvVariable("PASSWORD");
+
+        try {
+            // create and init user
+            sdk.createNewUser(utils.USERNAME_SATOSHI);
+            sdk.initializeUser(utils.USERNAME_SATOSHI);
+            System.out.println("Created and initialized new user.");
+
+            // create new wallet
+            sdk.setWalletPassword(utils.PIN, password);
+            sdk.createNewWallet(utils.PIN);
+            System.out.println("Created and initialized new wallet.");
+
+            // claim output
+            sdk.claimOutputs(utils.PIN);
+            System.out.println("claim outputs success");
+
+        } catch (Exception e) {
+            throw new RuntimeException("Claim outputs example failed", e);
+        }
+    }
+}

sdk/bindings/android/tests/src/main/java/com/cawaena/examples/MigrateWalletFromBackup05.java
@@ -1,0 +1,39 @@
+package com.cawaena.examples;
+
+import com.cawaena.Wallet;
+public class MigrateWalletFromBackup05 {
+
+    public static void main(String[] args) {
+
+        // initialize the sdk
+        Wallet sdk = utils.initSdk(utils.USERNAME_SATOSHI);
+
+        String password = utils.getEnvVariable("PASSWORD");
+
+        try {
+            // create and init user
+            sdk.createNewUser(utils.USERNAME_SATOSHI);
+            sdk.initializeUser(utils.USERNAME_SATOSHI);
+            System.out.println("Created and initialized new user.");
+
+            // create new wallet
+            sdk.setWalletPassword(utils.PIN, password);
+            sdk.createNewWallet(utils.PIN);
+            System.out.println("Created and initialized new wallet.");
+
+            // create backup
+            byte[] backup_bytes = sdk.createWalletBackup(utils.PIN, "backup_password");
+
+            // delete existing wallet
+            sdk.deleteWallet(utils.PIN);
+            System.out.println("deleted existing wallet");
+
+            // migrate wallet from backup
+            sdk.createWalletFromBackup(utils.PIN, backup_bytes, "backup_password");
+            System.out.println("wallet restored from backup");
+
+        } catch (Exception e) {
+            throw new RuntimeException("Migrate wallet from backup example failed", e);
+        }
+    }
+}