3a748fc3f8268e59b54ef99adf0cc549f772faaf
antbern <40672068+antbern@users.noreply.github.com>
2025-03-11 14:20:53 +0000

    update build.rs

--- sdk/build.rs
+++ sdk/build.rs
@@ -1,7 +1,8 @@
 /// This file configures and generates metadata for the SDK build process, including information
 /// about the environment, version, and other build-related properties.
-fn main() -> shadow_rs::SdResult<()> {
+fn main() {
     // Create a default deny set which excludes `CARGO_METADATA`.
+
     let mut deny = shadow_rs::default_deny();
 
     // Exclude additional unnecessary properties.
@@ -10,6 +11,5 @@ fn main() -> shadow_rs::SdResult<()> {
     deny.insert(shadow_rs::COMMIT_AUTHOR);
     deny.insert(shadow_rs::COMMIT_EMAIL);
 
-    // Generate the build information, excluding the properties specified in `deny`.
-    shadow_rs::new_deny(deny)
+    shadow_rs::ShadowBuilder::builder().deny_const(deny).build().unwrap();
 }
