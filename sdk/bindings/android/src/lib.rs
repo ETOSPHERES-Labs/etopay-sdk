89ba2863658c34d33431a05105617fc00f53cd1c
l.szczepanski <l.szczepanski@etogruppe.com>
2025-03-12 14:28:56 +0000

    reverting android bindings

--- sdk/bindings/android/src/lib.rs
+++ sdk/bindings/android/src/lib.rs
@@ -104,13 +104,16 @@ mod ffi {
     /// Fetch available currencies and corresponding node urls.
     ///
     /// @return Serialized string of a hashmap with currencies as key and node urls as value
-    pub fn getNetworks() -> Result<(), String> {
+    pub fn getNetworks() -> Result<String, String> {
         let result = runtime().block_on(async move {
             let mut sdk = get_or_init_sdk().write().await;
             sdk.get_networks().await
         });
 
-        result.map_err(|e| format!("{e:#?}"))
+        match result {
+            Ok(value) => serde_json::to_string(&value).map_err(|e| format!("{e:#?}")),
+            Err(e) => Err(format!("{e:#?}")),
+        }
     }
 
     /// Selects the network for the Cryptpay SDK.
