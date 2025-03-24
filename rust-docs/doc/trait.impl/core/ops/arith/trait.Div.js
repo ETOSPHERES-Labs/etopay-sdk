1443d5cced692587a19e07751e709783aeeb41b6
antbern <antbern@users.noreply.github.com>
2025-03-24 08:29:05 +0000

    deploy: afc29f70eb2a8a2fd5a7339c8510a74d3e5b8cb7

--- rust-docs/doc/trait.impl/core/ops/arith/trait.Div.js
+++ rust-docs/doc/trait.impl/core/ops/arith/trait.Div.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["etopay_sdk",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/ops/arith/trait.Div.html\" title=\"trait core::ops::arith::Div\">Div</a> for <a class=\"struct\" href=\"etopay_sdk/types/currencies/struct.CryptoAmount.html\" title=\"struct etopay_sdk::types::currencies::CryptoAmount\">CryptoAmount</a>"]]]]);
+    var implementors = Object.fromEntries([["etopay_sdk",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/ops/arith/trait.Div.html\" title=\"trait core::ops::arith::Div\">Div</a> for <a class=\"struct\" href=\"etopay_sdk/types/currencies/struct.CryptoAmount.html\" title=\"struct etopay_sdk::types::currencies::CryptoAmount\">CryptoAmount</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- rust-docs/doc/trait.impl/core/ops/deref/trait.DerefMut.js
+++ rust-docs/doc/trait.impl/core/ops/deref/trait.DerefMut.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["etopay_sdk",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.83.0/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"etopay_sdk/wallet_manager/struct.WalletBorrow.html\" title=\"struct etopay_sdk::wallet_manager::WalletBorrow\">WalletBorrow</a>&lt;'_&gt;"]]]]);
+    var implementors = Object.fromEntries([["etopay_sdk",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"etopay_sdk/wallet_manager/struct.WalletBorrow.html\" title=\"struct etopay_sdk::wallet_manager::WalletBorrow\">WalletBorrow</a>&lt;'_&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
