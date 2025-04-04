e8b8b90e096965e2b5fd6f94a5ad0381f82ab7d4
antbern <antbern@users.noreply.github.com>
2025-04-04 06:03:53 +0000

    deploy: 713a645cfdbf454f4a35c6892df2065b2180c153

--- rust-docs/doc/trait.impl/core/cmp/trait.PartialOrd.js
+++ rust-docs/doc/trait.impl/core/cmp/trait.PartialOrd.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["etopay_sdk",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"etopay_sdk/types/currencies/struct.CryptoAmount.html\" title=\"struct etopay_sdk::types::currencies::CryptoAmount\">CryptoAmount</a>"]]]]);
+    var implementors = Object.fromEntries([["etopay_sdk",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.86.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"etopay_sdk/types/currencies/struct.CryptoAmount.html\" title=\"struct etopay_sdk::types::currencies::CryptoAmount\">CryptoAmount</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
