4a59dcecee72ea66b49bdd5e819486396fb5de99
antbern <40672068+antbern@users.noreply.github.com>
2025-04-11 09:47:53 +0000

    return single version number from health endpoint

--- api_types/src/api/generic.rs
+++ api_types/src/api/generic.rs
@@ -13,14 +13,6 @@ pub enum ApiCryptoCurrency {
 /// Struct for storing the commit hash from different services
 #[derive(Debug, Deserialize, Serialize, Clone, PartialEq)]
 #[cfg_attr(feature = "utoipa", derive(utoipa::ToSchema))]
-pub struct HelloResponse {
-    pub message: String,
-    pub account_hash: String,
-    pub dlt_hash: String,
-    pub postident_hash: String,
-    pub transaction_hash: String,
-    pub viviswap_hash: String,
-    pub user_data_hash: String,
-    pub webhook_hash: String,
-    pub requests_aggregator_hash: String,
+pub struct HealthResponse {
+    pub version: String,
 }
