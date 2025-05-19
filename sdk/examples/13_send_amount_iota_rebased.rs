bd1bece9c2834298030c90813c6332380dd3e77b
l.szczepanski <l.szczepanski@etogruppe.com>
2025-05-19 09:41:44 +0000

    updated rust example

--- sdk/examples/13_send_amount_iota_rebased.rs
+++ sdk/examples/13_send_amount_iota_rebased.rs
@@ -1,36 +1,19 @@
 mod utils;
-use std::path::Path;
-
-use api_types::api::networks::{ApiNetwork, ApiProtocol};
-use etopay_sdk::{
-    core::{Config, Sdk},
-    types::newtypes::PlainPassword,
-};
+use etopay_sdk::types::newtypes::PlainPassword;
 use rust_decimal_macros::dec;
 use testing::USER_SATOSHI;
+use utils::init_sdk;
 
 #[allow(clippy::unwrap_used, clippy::expect_used)]
 #[tokio::main]
 async fn main() {
-    env_logger::builder().filter_level(log::LevelFilter::Info).init();
-
-    let user: utils::TestUser = (*USER_SATOSHI).clone().into();
-
-    let cleanup = testing::CleanUp::default();
-
     // Initialize SDK
-    let config = Config {
-        backend_url: "https://localhost/".parse().unwrap(),
-        path_prefix: Path::new(&cleanup.path_prefix).into(),
-        auth_provider: "standalone".to_string(),
-        log_level: log::LevelFilter::Debug,
-    };
-    let mut sdk = Sdk::new(config).expect("should not fail to initialize sdk");
+    let user: utils::TestUser = (*USER_SATOSHI).clone().into();
+    let (mut sdk, _cleanup) = init_sdk().await;
 
     // Create new user
     sdk.create_new_user(&user.username).await.unwrap();
     sdk.init_user(&user.username).await.unwrap();
-    sdk.refresh_access_token(None).await.unwrap();
 
     // Create / init new wallet from mnemonic
     sdk.set_wallet_password(
@@ -44,28 +27,25 @@ async fn main() {
         .unwrap();
 
     // Fetch networks from backend
-    let networks = sdk.get_networks().await.unwrap();
-    let iota_network_key = &networks.get(2).unwrap().key;
-    sdk.set_network(iota_network_key.to_string()).await.unwrap();
+    let _ = sdk.get_networks().await.unwrap();
+    sdk.set_network("iota_rebased_testnet".to_string()).await.unwrap();
 
     // Generate new address
     let recipient_address = sdk.generate_new_address(&user.pin).await.unwrap();
     let balance = sdk.get_balance(&user.pin).await.unwrap();
     println!("address: {recipient_address}, balance: {balance:?}");
 
-    let amount = dec!(1.0).try_into().unwrap();
+    // Send amount
+    let amount = dec!(2.0).try_into().unwrap();
     let data = Some("test".to_string().into_bytes());
-
-    // Estimate gas
+    // estimate gas
     let estimate = sdk
         .estimate_gas(&user.pin, &recipient_address, amount, data.clone())
         .await
-        .unwrap()
-        .gas_limit;
+        .unwrap();
 
     println!("Estimated gas: {estimate:?}");
 
-    // Send amount
     let tx_id = sdk
         .send_amount(&user.pin, &recipient_address, amount, data)
         .await
